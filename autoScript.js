4ec0c1a97960a0fdbaf37a24f6b2082f97ccd0c5
pignose305 <110445995+pignose305@users.noreply.github.com>
2022-08-17 23:51:45 -0700

    Add files via upload

---
+++ autoScript.js
@@ -0,0 +1,51 @@
+// Click 'Home'
+document.getElementById('m1').click()
+
+// Click 'Products+Services'
+document.getElementById('m2').click()
+
+// Click 'Product Catalog' under 'Products+Services'
+document.getElementById('m2_1').click()
+
+// Click 'Woman' under 'Product Catalog'
+document.getElementById('m2_3').click()
+
+// Scroll the page
+document.scrollTop(document.height());
+
+// Click 'add to cart'
+document.getElementById('tg_baqh').click()
+
+// Click 'Products+Services'
+document.getElementById('m2').click()
+
+// Click 'Man' under 'Product Catalog'
+document.getElementById('m2_4').click()
+
+// Scroll the page
+document.scrollTop(document.height());
+
+// Click 'add to cart'
+document.getElementById('tg_baqh').click()
+
+// Click 'Products+Services'
+document.getElementById('m2').click()
+
+// Click Featured Products under Products+Services
+document.getElementById('m2_2').click()
+
+// Click 'add to cart'
+document.getElementById('tg_baqh').click()
+
+// Click 'Registration'
+document.getElementById('m5').click()
+
+// Enter Data
+document.getElementById('Name').value += 'User1';
+document.getElementById('E-main').value += 'test@westcliff.edu';
+document.getElementById('Cell Phone').value += '2324567543';
+document.getElementById('Address').value += 'Westcliff';
+document.getElementById('City').value += 'Irvine';
+document.getElementById('Preferred Login Name').value += 'User1';
+document.getElementById('Login Password').value += 'password';
+document.getElementById('Submit').click()
